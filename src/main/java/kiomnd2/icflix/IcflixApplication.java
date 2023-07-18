c233b0e0bcb2054e872f4808adef54d3a6198d20
kiomnd2 <kiomnd2@gmail.com>
2023-07-18 20:24:34 +0900

    first commit

---
+++ src/main/java/kiomnd2/icflix/IcflixApplication.java
@@ -0,0 +1,13 @@
+package kiomnd2.icflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcflixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcflixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kiomnd2/icflix/IcflixApplicationTests.java
@@ -0,0 +1,13 @@
+package kiomnd2.icflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcflixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
