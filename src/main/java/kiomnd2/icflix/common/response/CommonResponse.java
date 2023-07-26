8822b4db3a45395d038b9d743a933c3452276d44
kiomnd2 <kiomnd2@gmail.com>
2023-07-26 20:44:50 +0900

    에러처리

--- src/main/java/kiomnd2/icflix/common/response/CommonResponse.java
+++ src/main/java/kiomnd2/icflix/common/response/CommonResponse.java
@@ -10,12 +10,17 @@ import lombok.NoArgsConstructor;
 public class CommonResponse<T> {
     private String code;
     private T body;
+    private String message;
 
     public static <T> CommonResponse<T>  success(T body) {
-        return new CommonResponse<>("0000", body);
+        return new CommonResponse<>("0000", body, null);
     }
 
     public static <T> CommonResponse<String> fail(ErrorCode errorCode) {
-        return new CommonResponse<>(errorCode.name(), errorCode.getMessage() );
+        return new CommonResponse<>(errorCode.name(), null, errorCode.getMessage());
+    }
+
+    public static <T> CommonResponse<String> fail(ErrorCode errorCode, Object ... args) {
+        return new CommonResponse<>(errorCode.name(), null, errorCode.getMessage(args));
     }
 }
--- src/main/java/kiomnd2/icflix/common/response/ResponseControllerAdvice.java
+++ src/main/java/kiomnd2/icflix/common/response/ResponseControllerAdvice.java
@@ -4,6 +4,9 @@ import com.sun.jdi.event.ExceptionEvent;
 import kiomnd2.icflix.common.response.exception.BaseException;
 import org.slf4j.MDC;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -36,5 +39,18 @@ public class ResponseControllerAdvice {
     @ExceptionHandler(value = BaseException.class)
     public CommonResponse<String> onBaseException(BaseException e) {
         return CommonResponse.fail(e.getErrorCode());
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public CommonResponse<String> onArgumentNotValidException(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        FieldError fieldError = bindingResult.getFieldError();
+        if (fieldError != null) {
+            return CommonResponse.fail(ErrorCode.COMMON_REQUIRED_VALUE, fieldError.getField());
+        } else {
+            return CommonResponse.fail(ErrorCode.COMMON_REQUIRED_VALUE, "UNKNOWN");
+        }
     }
 }
